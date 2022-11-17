bc5cd7c9a4e5b5022168e575740247464d6cc9a1
jpphilips <78326013+jpphilips@users.noreply.github.com>
2022-11-17 01:01:39 +0100

    ORM: implemented ORM to set up database

---
+++ backend/core/db/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column, ForeignKey, Integer, Float, DateTime, String, Boolean
+from sqlalchemy.orm import relationship
+from db.database import Base
+
+
+class Currency(Base):
+    __tablename__ = "currencies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    country = Column(String, unique=True, index=True)
+    isocode = Column(String, unique=True)
+    symbol = Column(String, unique=True)
+
+    rates = relationship(
+        "Rate", back_populates="currency")
+
+
+class Rate(Base):
+    __tablename__ = "rates"
+
+    id = Column(Integer, primary_key=True, index=True)
+    official_buy = Column(Float)
+    official_sell = Column(Float)
+    parallel_buy = Column(Float)
+    parallel_sell = Column(Float)
+    last_updated = Column(DateTime)
+    currency_id = Column(Integer, ForeignKey("currencies.id"))
+
+    currency = relationship("Currency", back_populates="rates")
+
+
+class Admin(Base):
+    __tablename__ = "admins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
--- backend/core/main.py
+++ backend/core/main.py
@@ -1,6 +1,28 @@
+from typing import List
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+
+from db import crude, models, schemas
+from db.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+get_db()
+
+app = FastAPI()
+
 
 @app.get("/")
 def index():
