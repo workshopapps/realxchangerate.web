803b222cbacd0f6b6cd5f2f0f77fddeffab4a5c8
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-11-17 10:30:42 +0100

    set the initial repo structure for admin service

---
+++ backend/admin_service/tests/test.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
+
+
+from admin_service.main import app
+
+client = TestClient(app)
+
+def test_index():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Hello Team Bevel - Admin Microservice'}
