15f6a3633dc7cb55e5f3f1683a4c008b01be02c3
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-11-19 00:21:31 +0100

    feat(BAC-7):built function to get all currencies from db

--- backend/app/api/v1/currency.py
+++ backend/app/api/v1/currency.py
@@ -7,3 +7,22 @@ from app import schemas, crud
 from app.api.deps import get_db
 
 router = APIRouter()
+
+
+@router.get("", response_model=List[schemas.Currency])
+def get_currencies(db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> Any:
+    """
+    get all currencies.
+    """
+    currency = crud.currency.get_multi(db, skip=skip, limit=limit)
+    return currency
+
+
+@router.post("", response_model=schemas.Currency)
+def add_currencies(*, db: Session = Depends(get_db), currency_in: schemas.CurrencyCreate) -> Any:
+    """
+    add new currencies.
+    """
+    currency = crud.currency.create(db, obj_in=currency_in)
+    return currency
+
