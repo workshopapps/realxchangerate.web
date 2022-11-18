5bb90404bcb5e28900681f40d145c318bd479145
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-11-18 12:20:50 +0100

    function to get all currencies in db

--- backend/admin_service/main.py
+++ backend/admin_service/main.py
@@ -1,8 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends 
+import schemas, models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
 
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()    
+
+models.Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def index():
     return {'message': 'Hello Team Bevel - Admin Microservice'}
+
+
+
+
+
+# function to get all currencies from database
+
+@app.get("/get_currencies")
+def all(db: Session = Depends(get_db))  :
+    currencies = db.query(models.Currency).all()
+    return currencies
+
+
+
