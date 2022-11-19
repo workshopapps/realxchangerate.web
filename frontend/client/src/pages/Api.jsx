b44506eb3026ca9a4bba3ef0ee3c45bdc33764f6
Victor Anyadioha <talk2okwundu@gmail.com>
2022-11-19 05:26:22 +0100

    Feat: Added Benefits component

--- frontend/client/src/pages/Api.jsx
+++ frontend/client/src/pages/Api.jsx
@@ -4,6 +4,7 @@ import heroImage from '../assets/Api page assets/heroImage.png'
 import padlock from '../assets/Api page assets/padlock.png'
 import rocket from '../assets/Api page assets/rocket.png'
 import dart from '../assets/Api page assets/dart.png'
+import bg from '../assets/Api page assets/bg.png'
 
 const ApiGlobalStyle = createGlobalStyle`
     *, *::before, *::after {
@@ -210,6 +211,50 @@ const Cards = () => {
         </StyledCards>
     )
 }
+const StyledBenefits = styled.section`
+  width: 100vw;
+  padding: 6rem 12rem;
+  background: linear-gradient(
+      0deg,
+      rgba(245, 246, 248, 0.3),
+      rgba(245, 246, 248, 0.3)
+    ),
+    url(${bg});
+  height: 65rem;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  div {
+    padding: 6rem 4rem;
+    background-color: #f8fafc;
+    width: 48rem;
+    border-radius: 0.8rem;
+    margin: auto 0;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  h5 {
+    font-size: 2.4rem;
+    margin-bottom: 3.2rem;
+  }
+  @media only screen and (max-width: 87rem) {
+    padding: 6rem 8rem;
+  }
+  @media only screen and (max-width: 42rem) {
+    padding: 4rem 2rem;
+    height: 56rem;
+    div {
+      padding: 2.4rem;
+      width: 100%;
+      position: static;
+      transform: translateY(0);
+    }
+    h5 {
+      font-size: 1.8rem;
+    }
+  }
+`;
 const Api = () => {
     return (
         <div className="fro-51-api">
@@ -227,6 +272,14 @@ const Api = () => {
                 </div>
             </StyledHero>
             <Cards />
+            <StyledBenefits>
+                <div>
+                    <h5>Real-time, Accurate and Credible Data for World Currencies</h5>
+                    <p>
+                        Auditors, accountants, hedge fund managers, treasurers, software developers and product managers use our API to track FX movements accurately and consistently. The ability to automate your exchange rate retrieval processes will put your auditors at ease and give your finance department accurate rates they can trust. The simple and flexible functionality of our API accurately tracks FX movements and eliminates manual errors. Supports JSON, XML and CSV formats.
+                    </p>
+                </div>
+            </StyledBenefits>
         </div>
     )
 }
