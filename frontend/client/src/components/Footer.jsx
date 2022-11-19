8b091355be5406f81e462964217b650f82285123
GREENFONTS <godwillonyewuchii@gmail.com>
2022-11-19 22:46:46 +0100

    fix: footer links

--- frontend/client/src/components/Footer.jsx
+++ frontend/client/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Link } from "@mui/material";
 import React from "react";
 import DropDown from "./DropDown";
 
