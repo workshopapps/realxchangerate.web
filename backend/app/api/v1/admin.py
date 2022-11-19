6de616eb1fe65bfa4f59fa6b8be64274661d5f1a
Lamido <uklamido@gmail.com>
2022-11-19 15:55:26 +0100

    feat(BAC-47 and BAC-37): added an endpoint that routes through the admin authentication to return the get_currencies function. added the street rate twitter bot

--- backend/app/api/v1/admin.py
+++ backend/app/api/v1/admin.py
@@ -5,12 +5,12 @@ from sqlalchemy.orm import Session
 
 from app import schemas, crud
 from app.api.deps import get_db
-from v1 import currency
-from v1 import auth
+from v1.currency import get_currencies
+from v1.auth import get_current_active_user
 
 router = APIRouter()
 
 #authenticates that the admin is logged in then returns all the currencies in the database with the get_currencies function
 @router.get("/currencies", response_model=List[schemas.Currency])
-async def currencies(current_user: user = Depends(get_current_active_user)):
+async def currencies(user = Depends(get_current_active_user)):
     return get_currencies()
