8091505f65479ad1feecabea2b688eb7afcd17d1
alisani081 <960395b7a1e79fe610d98498afeab15d68cbe064>
2022-11-19 16:31:25 +0100

    build an endpoint to fetch exchange rate from third party apis

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from app.api.v1 import currency
 from app.api.v1 import admin
 from app.api.v1 import rate
+from app.api.v1 import api_service
 
 api_router = APIRouter()
 api_router.include_router(currency.router, prefix="/currency", tags=["currencies"])
 api_router.include_router(admin.router, prefix="/admin", tags=["currencies"])
 api_router.include_router(rate.router, prefix="/rate", tags=["currencies"])
+api_router.include_router(api_service.router, prefix="/service", tags=["services"])
