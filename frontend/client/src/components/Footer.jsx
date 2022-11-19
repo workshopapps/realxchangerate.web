25feeb10fbbafe0aa8d6f1f87c0fb7fcf7970874
GREENFONTS <godwillonyewuchii@gmail.com>
2022-11-19 16:22:37 +0100

    Made some changes

--- frontend/client/src/components/Footer.jsx
+++ frontend/client/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Link, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 import DropDown from "./DropDown";
 
