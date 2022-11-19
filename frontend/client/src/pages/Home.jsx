438a5681e672c5c0c454c3d0e47874f2c1796052
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-11-19 08:16:44 +0100

    feat: completed the landing page

--- frontend/client/src/pages/Home.jsx
+++ frontend/client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Hero, LiveRate } from "../components";
+import { Hero, LiveRate, Table } from "../components";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <LiveRate />
+      <Table />
     </>
   );
 };
