c9cc0c94ce1b89f74fc4da21e4fec6b363115dc8
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-11-20 10:09:53 +0100

    fix

--- frontend/client/src/components/CareerButton.js
+++ frontend/client/src/components/CareerButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import down from "../pages/Careers/assets/down.svg";
+import down from "../Pages/Careers/assets/down.svg";
 function Button({ cta, children }) {
   return (
     <>
