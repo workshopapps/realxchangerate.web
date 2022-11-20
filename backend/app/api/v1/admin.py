a6befb4afc756f67aa2ab77a91aac7d2c3a7e8de
Lamido <uklamido@gmail.com>
2022-11-20 15:12:19 +0100

    feat(BAC-37): added an end point that calls the get_currencies function if the user is authenticated

--- backend/app/api/v1/admin.py
+++ backend/app/api/v1/admin.py
@@ -1,9 +1,21 @@
 from typing import Any, List
+from app import schemas
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app import schemas, crud
 from app.api.deps import get_db
+from .auth import get_current_active_user
+from .currency import get_currencies
 
 router = APIRouter()
+
+
+@router.post("/currencies", response_model=List[schemas.Currency], dependencies=[Depends(get_current_active_user)])
+async def get_all_currencies() -> Any:
+    """
+    this route calls the gets all currency function
+
+    """
+    return get_currencies()
