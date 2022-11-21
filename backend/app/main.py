a36be2202e2d9d46e1e6fcb291cbd6984ff8499d
EngrZaks <abzakariyya@gmail.com>
2022-11-21 16:21:19 +0100

    set cors to accept from our frontend origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,27 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api.v1 import getcurrency
 from app.api.v1 import api_router
 from app.core import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://team-bevel-client.netlify.app",
+    "https://exchange.hng.tech",
+    "https://exchange.hng.tech/*",
+    "https://team-bevel-client.netlify.app/*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.include_router(getcurrency.router)
