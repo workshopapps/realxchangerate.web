110ede763e67f9c317f4a6232fb05ec5ffbb2aa5
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-11-21 08:10:32 +0100

    fix

--- run.sh
+++ run.sh
@@ -12,8 +12,11 @@ sudo docker-compose down
 # wait for 20 seconds
 sleep 20
 
-# sudo docker-compose up
-sudo docker-compose up -d --force-recreate --no-deps --build
+#sudo docker-compose up -d
+
+sudo docker system prune -a -f
+
+sudo docker-compose up -d
 
 
 echo "Thank you for running with us"
