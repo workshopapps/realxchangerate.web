8b430694981b7b817865668558bdf4f26d748c3f
EngrZaks <abzakariyya@gmail.com>
2022-11-22 22:57:07 +0100

    added an endpoint for adding a new currency

--- backend/app/api/v1/admin.py
+++ backend/app/api/v1/admin.py
@@ -2,8 +2,24 @@ from typing import Any, List
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from app.models.rate import Rate
+from app.models.currency import Currency
 
 from app import schemas, crud
 from app.api.deps import get_db
 
 router = APIRouter()
+
+# @router.post("/add_currency")
+# def add_currency(isocode, db:Session = Depends(get_db)):
+
+
+@router.post("/add_currency")
+def post_currency(
+    *, db: Session = Depends(get_db), currency_in: schemas.CurrencyCreate
+) -> Any:
+    """
+    Create a new currency.
+    """
+    c = crud.currency.create(db, obj_in=currency_in)
+    return c
