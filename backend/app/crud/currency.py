923d2b4abc77920aafe37ebb1399e8ff5aba58b0
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-23 16:44:04 +0100

    Create endpoint to get all currencies from database

--- backend/app/crud/currency.py
+++ backend/app/crud/currency.py
@@ -9,9 +9,14 @@ from app.schemas.currency import CurrencyCreate, CurrencyUpdate
 
 
 class CRUDCurrency(CRUDBase[Currency, CurrencyCreate, CurrencyUpdate]):
-    # Declare model specific CRUD operation methods.
+    """ Declares model specific CRUD operation methods"""
+
     def get_currency_by_isocode(self, db:Session, isocode:str) -> Any:
         return db.query(Currency).filter(Currency.isocode == isocode).first()
+    
+    def get_all_currencies(self, db:Session):
+        """Returns all currencies from the database"""
+        return db.query(Currency).all()
     
     def delete_currency_by_isocode(self, db:Session, isocode:str) -> dict:
         ''' This function deletes the currency associated with the isocode passed in.
