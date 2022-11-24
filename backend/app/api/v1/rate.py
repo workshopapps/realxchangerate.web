123fe13d347c417204a6907e6834e8f0d7f53c0b
okputu julius <juliusstan10@gmail.com>
2022-11-24 01:07:31 +0100

    -refactor(BAC-61):Update Trend endpoint to return N <= 15 rates
    add function to query db for rates by limit in backend/app/crud/rate.py
    refactor (get) /history/{isocode} endpoint to return n <= 15 rates

--- backend/app/api/v1/rate.py
+++ backend/app/api/v1/rate.py
@@ -2,7 +2,7 @@ from typing import Any, List
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-
+from fastapi.responses import JSONResponse
 from app import schemas, crud
 from app.api.deps import get_db
 from app.models.rate import Rate
@@ -22,19 +22,28 @@ def getRate(isocode, db:Session = Depends(get_db)):
     return {"success":True, "status_code":200, "data": {"currency":currency, "rate":rate}}
 
     # """get the last 5 rates of a currency by its isocode."""
-@router.get('history/{isocode}')
-def getfiveRates(isocode, db:Session = Depends(get_db)):
+@router.get('/history/{isocode}')
+def get_rates_by_limit(isocode, db:Session = Depends(get_db),limit: int = 15):
+    #queries for currency based on isocode
     currency = crud.currency.get_currency_by_isocode(db, isocode=isocode)
+    
+    #return 404 status code if no currency was found
     if currency == None:
-        return {
-            "success":False, "message":"Currency not found", "status_code":404 
-        }
-    rate = db.query(Rate).filter(Rate.currency_id == currency.id).order_by(Rate.last_updated.desc()).all()[:5]
-    if len(rate) == 0:
-        return {
-            "success":False, "message":"No rate history found", "status_code":404 
-        }
-    return {"success":True, "status_code":200, "data": {"currency":currency, "rate":rate}}
+        raise HTTPException(status_code=404, detail="Currency not found")
+
+    #restrict limit to < 15 rate objects
+    if limit > 15:
+        raise HTTPException(status_code=400, detail="Not more than 15 rates per request")
+
+    #get rate objects based on limit set
+    rate = crud.rate.get_rates_by_limit(db,currency_id=currency.id, limit=limit)
+
+    #return no content if no rate object was found
+    if rate == None:
+        raise HTTPException(status_code=204, detail="No Content")
+     
+    #return response with status code 200, currency and list of rates 
+    return  {"status_code":200,"data": {"currency":currency, "rates":rate}}
 
 @router.get('/ip/{ip}')
 def get_ip_currency(ip, db:Session = Depends(get_db)):
@@ -50,7 +59,7 @@ def get_ip_currency(ip, db:Session = Depends(get_db)):
         }
     return {"success":True, "status_code":200, "data": {"currency":currency, "rate":rates}}
 
-router = APIRouter()
+# router = APIRouter()
 
 @router.get("", response_model=List[schemas.Rate])
 def get_rate(db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> Any:
--- backend/app/crud/rate.py
+++ backend/app/crud/rate.py
@@ -10,6 +10,15 @@ from app.schemas.rate import RateCreate, RateBase
 
 class CRUDRate(CRUDBase[Rate, RateCreate, RateBase]):
     # Declare model specific CRUD operation methods.
+    def get_rates_by_limit(self, db: Session, currency_id, skip: int = 0, limit: int = 15):
+        """
+        Queries the database and return rates of a 
+        particular currency based on the limit set
+
+        @currency_id: id of currency
+        @limit: no of rates to query for
+        """
+        return db.query(self.model).filter(self.model.currency_id == currency_id).order_by(self.model.last_updated.desc()).limit(limit).all()    
     pass
 
 rate = CRUDRate(Rate)
