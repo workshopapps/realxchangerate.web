e9fbdd856ef1446302e0bd0a583d7164a24236db
alisani081 <960395b7a1e79fe610d98498afeab15d68cbe064>
2022-11-24 09:48:35 +0100

    feat(BAC-66): created convert currency endpoint

--- backend/app/api/v1/rate.py
+++ backend/app/api/v1/rate.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.orm import Session
 
 from app import schemas, crud
@@ -8,6 +8,7 @@ from app.api.deps import get_db
 from app.models.rate import Rate
 from app.models.currency import Currency
 from app.api.deps import get_location
+
 router = APIRouter()
 
 #  get rates ofbject for a spcific isocode
@@ -72,3 +73,58 @@ def get_rate(db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="rates are not available at the moment")
     return rate
+
+
+@router.get("/convert")
+def convert_currency(
+    *,
+    db: Session = Depends(get_db),
+    from_currency: str = Query(max_length=3, min_length=3),
+    to_currency: str = Query(max_length=3, min_length=3),
+    amount: str,
+) -> Any:
+    """
+    This endpoint converts amount from one currency to another.
+    `amount`: The amount you want to covert
+    `from_currency`: The isocode of currency you want to convert from.
+    `to_currency`: The isocode of currency you want to convert to.
+    """
+    from_currency_in, to_currency_in = from_currency.upper(), to_currency.upper()
+    from_currency_obj = crud.currency.get_currency_by_isocode(
+        db, isocode=from_currency_in
+    )
+    to_currency_obj = crud.currency.get_currency_by_isocode(db, isocode=to_currency_in)
+    if from_currency_obj is None or to_currency_obj is None:
+        return {"success": False, "message": "Please send a valid currency isocode."}
+
+    try:
+        from_rate_obj = crud.rate.get_rate_by_currency_id(
+            db, currency_id=from_currency_obj.id
+        )
+        to_rate_obj = crud.rate.get_rate_by_currency_id(
+            db, currency_id=to_currency_obj.id
+        )
+        from_official_rate, to_official_rate = (
+            from_rate_obj.official_buy,
+            to_rate_obj.official_buy,
+        )
+        from_parallel_rate, to_parallel_rate = (
+            from_rate_obj.parallel_buy,
+            to_rate_obj.parallel_buy,
+        )
+
+        official_result = float(amount) / from_official_rate * to_official_rate
+        parallel_result = float(amount) / from_parallel_rate * to_parallel_rate
+
+        return {
+            "success": True,
+            "data": {
+                "amount": amount,
+                "from": from_currency_in,
+                "to": to_currency_in,
+                "parallel_total": "{:,.2f}".format(parallel_result),
+                "official_total": "{:,.2f}".format(official_result),
+            },
+        }
+    except:
+        return {"success": False, "message": "Failed to convert currencies."}
