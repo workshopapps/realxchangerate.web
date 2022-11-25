2cd0b8661744fb357e8e1ed424336cd379bb84a8
EngrZaks <abzakariyya@gmail.com>
2022-11-26 00:01:59 +0100

    style: styled home page and convert better. fix: updated backend endpoint

--- frontend/client/src/Pages/Home/Home.jsx
+++ frontend/client/src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, List, ListItem } from "@mui/material";
 import React from "react";
 import Convert from "../../components/home/Convert";
 import Hero from "../../components/home/Hero";
 import Table from "./components/Table";
+import Table2 from "./components/Table2";
+import { currenciesList } from "./data";
 
 const Home = () => {
   return (
@@ -22,13 +24,32 @@ const Home = () => {
           alignItems: "center",
           flexDirection: { xs: "column", sm: "row-reverse" },
           width: "100%",
-          gap: "1rem",
+          gap: "2.4rem",
         }}
       >
         <Convert />
         <Hero />
       </Box>
-      <Table />
+      {/* <Table /> */}
+      <List>
+        <ListItem
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            bgcolor: "#F1F5F9",
+            fontSize: "1.5rem",
+            fontWeight: 600,
+          }}
+        >
+          <Box>Currency</Box>
+          <Box>parallel Rate</Box>
+          <Box>Bank Rate</Box>
+        </ListItem>
+        {currenciesList.map((currency) => (
+          <Table2 isocode={currency.isocode} key={currency.id} />
+        ))}
+      </List>
     </Container>
   );
 };
