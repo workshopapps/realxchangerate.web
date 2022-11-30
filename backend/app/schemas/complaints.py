0b89019f1d7e4344bef81e992d7bd57daf219d7b
jpphilips <78326013+jpphilips@users.noreply.github.com>
2022-11-30 15:41:08 +0100

    resolved complaints conflicts

--- backend/app/schemas/complaints.py
+++ backend/app/schemas/complaints.py
@@ -3,12 +3,7 @@ from typing import Optional, Any
 from pydantic import BaseModel
 
 
-<<<<<<< HEAD
 class ComplaintBase(BaseModel):
-=======
-class Complaint(BaseModel):
-    id: int
->>>>>>> 04a703cdf26a22f5479090027375469f26c9c821
     full_name: str
     email: str
     complaint: str
@@ -20,6 +15,7 @@ class ComplaintCreate(ComplaintBase):
 
 class ComplaintUpdate(ComplaintBase):
     pass
+
 
 class Complaint(ComplaintBase):
     id: int
