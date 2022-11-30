8946fafd487c7c7d57a3c7876c384530910d7c00
damaestro165 <damaestro165@gmail.com>
2022-11-30 14:23:06 +0100

    feat:[STR-23] Link Glossary items to wikipedia page

---
+++ frontend/client/src/Pages/Glossary/GlossaryData.js
@@ -0,0 +1,373 @@
+export const boxes = [
+  {
+    name: 'A',
+    items: [
+      {
+        text: 'Agent',
+        link: 'https://en.wikipedia.org/wiki/Agent_(economics)',
+      },
+      {
+        text: 'Appreciation',
+        link: 'https://en.wikipedia.org/wiki/Currency_appreciation_and_depreciation',
+      },
+      {
+        text: 'Ask Rate',
+        link: 'https://en.wikipedia.org/wiki/Exchange_rate',
+      },
+      {
+        text: 'Aa3',
+        link: 'https://en.wikipedia.org/wiki/Moody%27s_Investors_Service',
+      },
+    ],
+  },
+
+  {
+    name: 'B',
+    items: [
+      {
+        text: 'Base Currency',
+        link: 'https://en.wikipedia.org/wiki/Currency_pair',
+      },
+      {
+        text: 'Bank Account',
+        link: 'https://en.wikipedia.org/wiki/Bank_account',
+      },
+      {
+        text: 'Buy Rate',
+        link: 'https://en.wikipedia.org/wiki/Exchange_rate',
+      },
+      { text: 'Bank Fee', link: 'https://en.wikipedia.org/wiki/Bank_charge' },
+    ],
+  },
+
+  {
+    name: 'C',
+    items: [
+      {
+        text: 'Central Bank',
+        link: 'https://en.wikipedia.org/wiki/Central_bank',
+      },
+      { text: 'Cheque', link: 'https://en.wikipedia.org/wiki/Cheque' },
+      {
+        text: 'Comission',
+        link: 'https://en.wikipedia.org/wiki/Commission_(remuneration)',
+      },
+      {
+        text: 'Credit Card',
+        link: 'https://en.wikipedia.org/wiki/Credit_card',
+      },
+    ],
+  },
+
+  {
+    name: 'D',
+    items: [
+      {
+        text: 'Debit card',
+        link: 'https://en.wikipedia.org/wiki/Debit_card',
+      },
+      {
+        text: 'Deposit',
+        link: 'https://en.wikipedia.org/wiki/Deposit_(finance)',
+      },
+      {
+        text: 'Delivary',
+        link: 'https://en.wikipedia.org/wiki/Delivery_(commerce)',
+      },
+      {
+        text: 'Dept',
+        link: 'https://en.wikipedia.org/wiki/Departmentalization',
+      },
+    ],
+  },
+
+  {
+    name: 'E',
+    items: [
+      {
+        text: 'ECB',
+        link: 'https://en.wikipedia.org/wiki/European_Central_Bank',
+      },
+      {
+        text: 'EBT',
+        link: 'https://en.wikipedia.org/wiki/Earnings_before_interest_and_taxes',
+      },
+      {
+        text: 'EBIT',
+        link: 'https://en.wikipedia.org/wiki/Earnings_before_interest_and_taxes',
+      },
+      {
+        text: 'EPS',
+        link: 'https://en.wikipedia.org/wiki/Earnings_per_share',
+      },
+    ],
+  },
+
+  {
+    name: 'F',
+    items: [
+      { text: 'Fee', link: 'https://en.wikipedia.org/wiki/Bank_charge' },
+      {
+        text: 'FDI',
+        link: 'https://en.wikipedia.org/wiki/Foreign_direct_investment',
+      },
+      {
+        text: 'Fair Trade',
+        link: 'https://en.wikipedia.org/wiki/Fair_trade',
+      },
+      {
+        text: 'Foreign Exchange',
+        link: 'https://en.wikipedia.org/wiki/Foreign_exchange_market',
+      },
+    ],
+  },
+
+  {
+    name: 'G',
+    items: [
+      {
+        text: 'Gold Standard',
+        link: 'https://en.wikipedia.org/wiki/Gold_standard',
+      },
+      {
+        text: 'GDP',
+        link: 'https://en.wikipedia.org/wiki/Gross_domestic_product',
+      },
+      {
+        text: 'GNP',
+        link: 'https://en.wikipedia.org/wiki/Gross_national_income',
+      },
+      {
+        text: 'GOP',
+        link: 'https://en.wikipedia.org/wiki/Republican_Party_(United_States)',
+      },
+    ],
+  },
+
+  {
+    name: 'H',
+    items: [
+      { text: 'Hot Money', link: 'https://en.wikipedia.org/wiki/Hot_money' },
+      { text: 'HSI', link: 'https://en.wikipedia.org/wiki/Hang_Seng_Index' },
+      {
+        text: 'Hard Currency',
+        link: 'https://en.wikipedia.org/wiki/Hard_currencyt',
+      },
+      {
+        text: 'Hard inquiry',
+        link: 'https://www.investopedia.com/terms/h/hard-inquiry.asp',
+      },
+    ],
+  },
+  {
+    name: 'I',
+    items: [
+      { text: 'Income', link: 'https://en.wikipedia.org/wiki/Income' },
+      { text: 'Inflation', link: 'https://en.wikipedia.org/wiki/Inflation' },
+      { text: 'Interest', link: 'https://en.wikipedia.org/wiki/Interest' },
+      { text: 'inventory', link: 'https://en.wikipedia.org/wiki/Inventory' },
+    ],
+  },
+  {
+    name: 'J',
+    items: [
+      { text: 'JV', link: 'https://en.wikipedia.org/wiki/Joint_venture' },
+      {
+        text: 'Jumbo CD',
+        link: 'https://en.wikipedia.org/wiki/Certificate_of_deposit',
+      },
+      {
+        text: 'Junk Bond',
+        link: 'https://en.wikipedia.org/wiki/High-yield_debt',
+      },
+      {
+        text: 'Jit',
+        link: 'https://en.wikipedia.org/wiki/Just-in-time_compilation',
+      },
+    ],
+  },
+  {
+    name: 'K',
+    items: [
+      {
+        text: 'KPI',
+        link: 'https://en.wikipedia.org/wiki/Performance_indicator',
+      },
+      {
+        text: 'Kernel',
+        link: 'https://en.wikipedia.org/wiki/Kernel_(algebra)',
+      },
+      {
+        text: 'Key Currency',
+        link: 'https://en.wikipedia.org/wiki/Exchange_rate',
+      },
+      { text: 'Key Ring', link: '' },
+    ],
+  },
+  {
+    name: 'L',
+    items: [
+      {
+        text: 'Liquidity',
+        link: 'https://en.wikipedia.org/wiki/Market_liquidity',
+      },
+      { text: 'Lock-in', link: 'https://en.wiktionary.org/wiki/lock-in' },
+      {
+        text: 'Long Run',
+        link: 'https://en.wikipedia.org/wiki/Long_run_and_short_run',
+      },
+      {
+        text: 'Limit Order',
+        link: 'https://en.wikipedia.org/wiki/Order_(exchange)#Limit_order',
+      },
+    ],
+  },
+  {
+    name: 'M',
+    items: [
+      { text: 'MF', link: 'https://en.wikipedia.org/wiki/Mutual_fund' },
+      { text: 'Materials', link: 'https://en.wikipedia.org/wiki/Material' },
+      {
+        text: 'Market Risk',
+        link: 'https://en.wikipedia.org/wiki/Market_risk',
+      },
+      {
+        text: 'Mortgage',
+        link: 'https://en.wikipedia.org/wiki/Mortgage_loan',
+      },
+    ],
+  },
+  {
+    name: 'N',
+    items: [
+      {
+        text: 'Net Income',
+        link: 'https://en.wikipedia.org/wiki/Net_income',
+      },
+      { text: 'Net Sale', link: 'https://en.wikipedia.org/wiki/Revenue' },
+      { text: 'Net Worth', link: 'https://en.wikipedia.org/wiki/Net_worth' },
+      {
+        text: 'NPA',
+        link: 'https://en.wikipedia.org/wiki/Non-performing_loan',
+      },
+    ],
+  },
+  {
+    name: 'O',
+    items: [
+      {
+        text: 'Object Cost',
+        link: 'https://en.wikipedia.org/wiki/Cost_object',
+      },
+      {
+        text: 'OEI',
+        link: 'https://en.wikipedia.org/wiki/Organization_of_Ibero-American_States',
+      },
+      {
+        text: 'OEM',
+        link: 'https://en.wikipedia.org/wiki/Original_equipment_manufacturer#Economies_of_scale',
+      },
+      {
+        text: 'OAC',
+        link: 'https://en.wikipedia.org/wiki/Object_Action_Complex',
+      },
+    ],
+  },
+  {
+    name: 'P',
+    items: [
+      { text: 'Premium', link: 'https://en.wikipedia.org/wiki/Risk_premium' },
+      {
+        text: 'Preferred Stock',
+        link: 'https://en.wikipedia.org/wiki/Preferred_stock',
+      },
+      { text: 'Policy Terms', link: 'https://en.wikipedia.org/wiki/Policy' },
+      {
+        text: 'Profit',
+        link: 'https://en.wikipedia.org/wiki/Profit_(economics)',
+      },
+    ],
+  },
+  {
+    name: 'Q',
+    items: [
+      {
+        text: 'QDRO',
+        link: 'https://en.wikipedia.org/wiki/Qualified_domestic_relations_order',
+      },
+      {
+        text: 'Quote',
+        link: 'https://en.wikipedia.org/wiki/Agent',
+      },
+      { text: 'Quotation', link: 'https://en.wikipedia.org/wiki/Quotation' },
+      {
+        text: 'Qubes',
+        link: 'https://financial-dictionary.thefreedictionary.com/Qubes',
+      },
+    ],
+  },
+  {
+    name: 'R',
+    items: [
+      { text: 'Ratio', link: 'https://en.wikipedia.org/wiki/Ratio' },
+      {
+        text: 'RBI',
+        link: 'https://en.wikipedia.org/wiki/Reserve_Bank_of_India',
+      },
+      {
+        text: 'ROI',
+        link: 'https://en.wikipedia.org/wiki/Return_on_investment',
+      },
+      {
+        text: 'Repo',
+        link: 'https://en.wikipedia.org/wiki/Repurchase_agreement',
+      },
+    ],
+  },
+  {
+    name: 'S',
+    items: [
+      {
+        text: 'Shares',
+        link: 'https://en.wikipedia.org/wiki/Share_(finance)',
+      },
+      {
+        text: 'Sharp Ration',
+        link: 'https://fsinvestments.com/fs-insights/chart-of-the-week-2022-1-14-value-growth-returns/',
+      },
+      {
+        text: 'Solvancy',
+        link: 'https://www.investopedia.com/terms/s/solvency.asp',
+      },
+      {
+        text: 'Sharesholder',
+        link: 'https://en.wikipedia.org/wiki/Shareholder',
+      },
+    ],
+  },
+  {
+    name: 'T',
+    items: [
+      {
+        text: 'Trust',
+        link: 'https://en.wikipedia.org/wiki/Trust_(business)',
+      },
+      {
+        text: 'Turns',
+        link: 'https://en.wikipedia.org/wiki/Inventory_turnover',
+      },
+      {
+        text: 'Tick',
+        link: 'https://www.investopedia.com/terms/t/tick.asp#:~:text=A%20tick%20is%20a%20measure,above%20%241%20is%20one%20cent.',
+      },
+      { text: 'TAFR', link: 'https://en.wikipedia.org/wiki/Trust-fund_tax' },
+    ],
+  },
+
+  { name: 'U' },
+  { name: 'V' },
+  { name: 'W' },
+  { name: 'X' },
+  { name: 'Y' },
+  { name: 'Z' },
+];
