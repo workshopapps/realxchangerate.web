09308a869018f73d473931def6a5b6806fe4692d
EngrZaks <abzakariyya@gmail.com>
2022-12-01 02:37:11 +0100

    feat: created endpoint for storing user complaints

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -10,6 +10,7 @@ from app.api.v1 import password_email_reset
 
 from app.api.v1 import api_service
 from app.api.v1 import news
+from app.api.v1 import complaints
 
 api_router = APIRouter()
 
@@ -24,3 +25,5 @@ api_router.include_router(password_email_reset.router,
 api_router.include_router(
     api_service.router, prefix="/service", tags=["services"])
 api_router.include_router(news.router, prefix="/news", tags=["news"])
+
+api_router.include_router(complaints.router, prefix="/complaints", tags=["complaints"])
