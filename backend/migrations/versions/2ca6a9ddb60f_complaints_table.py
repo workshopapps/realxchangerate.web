3c55a2ac7cea595c71d59782c1766624fe4bfde6
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-12-01 16:51:26 +0100

    Update complaints table to include status column

---
+++ backend/migrations/versions/2ca6a9ddb60f_complaints_table.py
@@ -0,0 +1,24 @@
+"""Complaints table
+
+Revision ID: 2ca6a9ddb60f
+Revises: 2d9db35f9c61, 830fd90eda57, b74d5e1da3af
+Create Date: 2022-12-01 16:32:38.235844
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2ca6a9ddb60f'
+down_revision = ('2d9db35f9c61', '830fd90eda57', 'b74d5e1da3af')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
