57209ccd7d8810953a5b3dcffcc0cf50b8abf958
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-12-02 08:06:53 +0100

    pulled backend

--- backend/migrations/versions/21f66bba6401_database_tables.py
+++ backend/migrations/versions/21f66bba6401_database_tables.py
@@ -1,8 +1,8 @@
 """database tables
 
-Revision ID: 7b1ad1b6e274
-Revises: 4c4680e49690
-Create Date: 2022-12-01 23:41:38.464202
+Revision ID: 21f66bba6401
+Revises: 
+Create Date: 2022-12-02 00:03:43.026975
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7b1ad1b6e274'
-down_revision = '4c4680e49690'
+revision = '21f66bba6401'
+down_revision = None
 branch_labels = None
 depends_on = None
 
