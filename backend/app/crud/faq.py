6476ff6c00d2af7b5e7d8d4de58c5b6ea8d2ef36
Genevieve Ikechukwu <ikechukwummachukwu2020@gmail.com>
2022-12-02 07:11:02 +0100

    updated the query for faq search to return search by words and letters

--- backend/app/crud/faq.py
+++ backend/app/crud/faq.py
@@ -1,4 +1,5 @@
 from typing import Optional, List, Any
+from sqlalchemy.sql.operators import ilike_op
 
 from sqlalchemy.orm import Session
 
@@ -11,7 +12,8 @@ from app.schemas.faq import FaqCreate, FaqUpdate
 
 class CRUDFaq(CRUDBase[Faq, FaqCreate, FaqUpdate]):
     def get_faq(self, db: Session, question: str) -> Any:
-        return db.query(Faq).filter_by(question=question).all()
+        look_for = '%{0}%'.format(question)
+        return db.query(Faq).filter(ilike_op(Faq.question, look_for)).all()
     
 
 faq = CRUDFaq(Faq)
