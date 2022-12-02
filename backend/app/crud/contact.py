8c2edc44bc64591ebc67dce10eda9cfe97eb69df
Lamido <uklamido@gmail.com>
2022-12-02 01:16:11 +0100

    BAC-29-a-GET-endpoint-that-gets-user-details-from-contact-us-table-in-the-database

--- backend/app/crud/contact.py
+++ backend/app/crud/contact.py
@@ -1,3 +1,5 @@
+from typing import Optional, List, Any
+from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
 from app.models.contact import Contact
 
@@ -5,7 +7,9 @@ from app.schemas.contact import ContactCreate, ContactUpdate
 
 
 class CRUDContact(CRUDBase[Contact, ContactCreate, ContactUpdate]):
-    pass
+    def get_all_contact_details(self, db: Session):
+        """Returns all contact details from the database"""
+        return db.query(Contact).all()
 
 
 contact = CRUDContact(Contact)
