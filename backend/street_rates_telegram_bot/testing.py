ea09f8af5ee20d98b035ca5c41898d66531fb71f
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-12-02 15:34:29 +0100

    added convert to another currency

--- backend/street_rates_telegram_bot/testing.py
+++ backend/street_rates_telegram_bot/testing.py
@@ -23,30 +23,56 @@ def help(update,context):
 /start -> Welcome to the channel
 /help -> View all the Commands
 /usd -> You type in the isocode of the currency and it returns the black market rate of the currency
+/convert -> You type in the isocode of the currency you want to convert then the isocode of the currency you want to convert to.
+e.g /convert USD NGN
 """)
 
 async def usd(update,context):
     value = "".join(context.args).upper()
     if value not in iso_code_list:
+        update.message.reply_text("This isocode does not have a black market rate")
+    else:
+        resp_data = await get_binancep2p_rate(value)
+        formated_data = await format_binance_response_data(resp_data)
+        parallel_buy = formated_data["buy_rate"]
+        reply = f"1 {value} to USD is {parallel_buy}"
+        update.message.reply_text(reply)
+
+async def convert(update,context):
+    from_currency = "".join(context.args[0]).upper()
+    to_currency = "".join(context.args[1]).upper()
+    if from_currency not in iso_code_list and to_currency not in iso_code_list:
         pass
 
-    resp_data = await get_binancep2p_rate(value)
-    formated_data = await format_binance_response_data(resp_data)
-    parallel_buy = formated_data["buy_rate"]
-    reply = f"1 {value} to USD is {parallel_buy}"
+    resp_data1 = await get_binancep2p_rate(from_currency)
+    formated_data1 = await format_binance_response_data(resp_data1)
+    resp_data2 = await get_binancep2p_rate(to_currency)
+    formated_data2 = await format_binance_response_data(resp_data2)
+    parallel_buy1 = formated_data1["buy_rate"]
+    parallel_buy2 = formated_data2["buy_rate"]
+    final_result = round(float(parallel_buy2) / float(parallel_buy1),2)
+    reply = f"1 {from_currency} to {to_currency} is {final_result}"
     update.message.reply_text(reply)
 
 def loop_runner(action):
     loop = asyncio.new_event_loop()
     return loop.run_until_complete(action)
 
+def loop_runner_2(action):
+    loop = asyncio.new_event_loop()
+    return loop.run_until_complete(action)
+
 def sync_run(update,context):
     return loop_runner(usd(update,context))
+
+def sync_run_2(update,context):
+    return loop_runner_2(convert(update,context))
 
 
 dispatcher.add_handler(telegram.ext.CommandHandler('start',start))
 dispatcher.add_handler(telegram.ext.CommandHandler('help',help))
 dispatcher.add_handler(telegram.ext.CommandHandler('usd',sync_run))
+dispatcher.add_handler(telegram.ext.CommandHandler('convert',sync_run_2))
 
 if __name__ == "__main__":
     updater.start_polling()
