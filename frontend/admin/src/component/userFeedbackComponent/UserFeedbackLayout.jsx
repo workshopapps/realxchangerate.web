245770acfbe09285944919d7bccf21ea8c416dcc
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-12-02 22:16:59 +0100

    update

--- frontend/admin/src/component/userFeedbackComponent/UserFeedbackLayout.jsx
+++ frontend/admin/src/component/userFeedbackComponent/UserFeedbackLayout.jsx
@@ -1,26 +1,23 @@
 import React, { useEffect } from 'react';
 import { Card, CardSkeleton } from './card/Card';
 import { StyledCardsWrapper, StyledWrapper } from './UserFeedback.styled';
-// import { Data } from '../../pages/userFeedbacks/mockData';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContacts } from '../../store/actions/contactActions';
 
 export default function UserFeedbackLayout() {
 	const dispatch = useDispatch();
 	const { contacts, requestStatus } = useSelector((state) => state.contacts);
-
 	const [data, setData] = React.useState([]);
+
 	useEffect(() => {
 		dispatch(getContacts());
 		setData(contacts);
-
 		//eslint-disable-next-line
 	}, []);
 
 	return (
 		<StyledWrapper>
 			<h2>Users’ Contact Us Information</h2>
-
 			<StyledCardsWrapper>
 				{requestStatus === 'pending' ? (
 					<>
@@ -29,13 +26,15 @@ export default function UserFeedbackLayout() {
 						<CardSkeleton />
 						<CardSkeleton />
 					</>
-				) : (
-					data
-						.slice(1)
-						.map((item, index) => (
-							<Card key={index} data={item} stats={item.status} />
-						))
-				)}
+				) : null}
+
+				{requestStatus === 'success'
+					? data
+							// .slice(1)
+							.map((item, index) => (
+								<Card key={index} data={item} stats={item.status} />
+							))
+					: null}
 			</StyledCardsWrapper>
 		</StyledWrapper>
 	);
