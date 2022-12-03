f0db20bfeae31885cb6b3c7ac4ab49e074348aed
Fathi <babayejuadesina@gmail.com>
2022-12-03 16:10:51 +0100

    feat: set stage for all input fields

--- frontend/client/src/Pages/StudentAmbassadorForm/components/ApplicationForm.jsx
+++ frontend/client/src/Pages/StudentAmbassadorForm/components/ApplicationForm.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormSection } from "../style/Application.style";
 import leftIcon from "../../../assets/svg/arrow-left.svg";
 import InputField from "../../../components/shared/InputField/InputField";
 import { Link } from "react-router-dom";
 
 const ApplicationForm = () => {
+  const [ambassadorDetails, setAmbassadorDetails] = useState({
+    firstName: "",
+    lastName: "",
+    DOB: "",
+    phoneNumber: "",
+    email: "",
+    address: "",
+    city: "",
+    country: "",
+    schoolName: "",
+    courseName: "",
+    entryYear: "",
+    completionYear: "",
+    howDidYouLearnAboutUs: "",
+    reasonForApplication: "",
+  });
+  const {
+    firstName,
+    lastName,
+    DOB,
+    phoneNumber,
+    email,
+    address,
+    city,
+    country,
+    schoolName,
+    courseName,
+    entryYear,
+    completionYear,
+    howDidYouLearnAboutUs,
+    reasonForApplication,
+  } = ambassadorDetails;
+
+  const [statusMessage, setStatusMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAmbassadorDetails({ ...ambassadorDetails, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ambassadorDetails);
+  };
+
   return (
     <FormSection>
       <div className="header">
@@ -21,22 +65,38 @@ const ApplicationForm = () => {
               placeHolder="First name"
               name="firstName"
               label="Name"
+              value={firstName}
+              handleChange={handleChange}
             />
           </div>
           <div>
-            <InputField type="text" placeHolder="Last name" name="lastName" />
+            <InputField
+              type="text"
+              placeHolder="Last name"
+              name="lastName"
+              value={lastName}
+              handleChange={handleChange}
+            />
           </div>
         </div>
         <div className="grid-two">
           <div>
-            <InputField type="date" name="dateOfBirth" label="Date of birth" />
+            <InputField
+              type="date"
+              name="DOB"
+              label="Date of birth"
+              value={DOB}
+              handleChange={handleChange}
+            />
           </div>
           <div>
             <InputField
               type="text"
               placeHolder="Mobile number"
-              name="phone"
+              name="phoneNumber"
               label="Phone number"
+              value={phoneNumber}
+              handleChange={handleChange}
             />
           </div>
         </div>
@@ -47,6 +107,8 @@ const ApplicationForm = () => {
               placeHolder="Email address"
               name="email"
               label="Email"
+              value={email}
+              handleChange={handleChange}
             />
           </div>
         </div>
@@ -58,17 +120,27 @@ const ApplicationForm = () => {
               placeHolder=""
               name="address"
               label="Address"
+              value={address}
+              handleChange={handleChange}
             />
           </div>
         </div>
         <div className="grid-two">
           <div>
-            <InputField type="text" name="city" label="City" />
+            <InputField
+              type="text"
+              name="city"
+              label="City"
+              value={city}
+              handleChange={handleChange}
+            />
           </div>
           <div className="select-field">
             <label htmlFor="country">Country</label>
             <select name="country" id="country">
-              <option value="">Country</option>
+              <option value={country} handleChange={handleChange}>
+                {country}
+              </option>
             </select>
           </div>
         </div>
@@ -78,47 +150,74 @@ const ApplicationForm = () => {
             <InputField
               type="text"
               placeHolder=""
-              name="school name"
+              name="schoolName"
               label="School name"
+              value={schoolName}
+              handleChange={handleChange}
             />
           </div>
         </div>
         <div className="grid-half course-name">
           <div>
-            <InputField type="text" name="course name" label="Name of course" />
+            <InputField
+              type="text"
+              name="courseName"
+              label="Name of course"
+              value={courseName}
+              handleChange={handleChange}
+            />
           </div>
         </div>
         <div className="grid-two course-year">
           <div>
-            <InputField type="text" name="entry year" label="Year of entry" />
+            <InputField
+              type="text"
+              name="entryYear"
+              label="Year of entry"
+              value={entryYear}
+              handleChange={handleChange}
+            />
           </div>
           <div>
             <InputField
               type="text"
-              name="completion year"
+              name="completionYear"
               label="Year of completion"
+              value={completionYear}
+              handleChange={handleChange}
             />
           </div>
         </div>
 
         <div className="text-area">
-          <label htmlFor="inquiry">
+          <label htmlFor="howDidYouLearnAboutUs">
             How did you find out about the Student Ambassador Program?
           </label>
-          <textarea name="inquiry" id="inquiry" cols="30" rows="10"></textarea>
+          <textarea
+            name="howDidYouLearnAboutUs"
+            id="howDidYouLearnAboutUs"
+            cols="30"
+            rows="10"
+            value={howDidYouLearnAboutUs}
+            handleChange={handleChange}
+          />
         </div>
         <div className="text-area">
-          <label htmlFor="ambassador">
+          <label htmlFor="reasonForApplication">
             Why do you want to be an student ambassador of our company?
           </label>
           <textarea
-            name="ambassador"
-            id="ambassador"
+            name="reasonForApplication"
+            id="reasonForApplication"
             cols="30"
             rows="10"
-          ></textarea>
+            value={reasonForApplication}
+            handleChange={handleChange}
+          />
         </div>
-        <button type="submit">Apply</button>
+        <button type="submit" onClick={handleSubmit}>
+          Apply
+        </button>
       </form>
     </FormSection>
   );
