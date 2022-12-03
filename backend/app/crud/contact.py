f208e2e2edda94bcdf90c415f7068c22dc5f42c0
Lamido <uklamido@gmail.com>
2022-12-03 14:18:29 +0100

    A-GET-endpoint-to-return-contact-us-details-for-individual-user-using-the-id

--- backend/app/crud/contact.py
+++ backend/app/crud/contact.py
@@ -2,12 +2,17 @@ from app.crud.base import CRUDBase
 from app.models.contact import Contact
 from sqlalchemy.orm import Session
 from app.schemas.contact import ContactCreate, ContactUpdate
+from typing import Any
 
 
 class CRUDContact(CRUDBase[Contact, ContactCreate, ContactUpdate]):
     def get_all_contact_details(self, db: Session):
         """Returns all contact details from the database"""
         return db.query(Contact).all()
+
+    def get_currency_by_id(self, db: Session, id: int) -> Any:
+        """Returns a specific contact detail using the contact id"""
+        return db.query(contact).filter(contact.id == id).first()
 
 
 contact = CRUDContact(Contact)
