2c04c15fca31ad52edf5582b43c0fd9082d740cd
GREENFONTS <godwillonyewuchii@gmail.com>
2022-12-04 03:48:14 +0100

    chore: clean the project foler

--- frontend/client/src/components/index.js
+++ frontend/client/src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Home } from "./Home";
 export { default as Countries } from "./Countries";
 export { default as Hero } from "./home/Hero";
 export { default as Table } from "./home/Table";
