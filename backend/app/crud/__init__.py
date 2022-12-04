1b65fc33bd1eb0e584bb448c7303f0cb49689f02
Genevieve Ikechukwu <ikechukwummachukwu2020@gmail.com>
2022-12-04 20:13:29 +0100

    Created an Adverts table and Post endpoint for adverts table

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -5,3 +5,4 @@ from app.crud.complaints import complaint
 from app.crud.faq import faq
 from app.crud.contact import contact
 from app.crud.student import student
+from app.crud.adverts import advert
---
+++ backend/app/crud/adverts.py
@@ -0,0 +1,28 @@
+from typing import Optional, List, Any
+
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from fastapi.encoders import jsonable_encoder
+
+from app.models.adverts import Advert
+from app.schemas.adverts import AdvertCreate, AdvertUpdate
+
+
+class CRUDAdvert(CRUDBase[Advert, AdvertCreate, AdvertUpdate]):
+    """
+    Advert Crud
+    """
+
+    def create(self, db:Session, obj_in: AdvertCreate) -> Advert:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_oj = Advert(
+          **obj_in_data
+        )
+        
+        db.add(db_oj)
+        db.commit()
+        db.refresh(db_oj)
+        return db_oj
+
+advert=CRUDAdvert(Advert)
