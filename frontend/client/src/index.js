51afdccaddb38893525eedd4138549719dd359ef
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-12-06 04:28:40 +0100

    working

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -7,6 +7,20 @@ import reportWebVitals from "./reportWebVitals";
 import { store } from "./redux/store";
 import "./translation/i18n";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+process.env.NODE_ENV == "production" && Sentry.init({
+  dsn: "https://b8eb1a11ad654e99994c510a60ddb487@o4504276826914816.ingest.sentry.io/4504278718152704",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
