5b41f36c1097199df240723b423b08650a971cb8
Genevieve Ikechukwu <ikechukwummachukwu2020@gmail.com>
2022-12-08 08:21:52 -0800

    send_email fixed

--- backend/app/api/v1/gmail.py
+++ backend/app/api/v1/gmail.py
@@ -1,52 +1,43 @@
-from __future__ import print_function
-
-import base64
 from email.message import EmailMessage
-
-import google.auth
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-
+import os
+import ssl
+import smtplib
+from decouple import config
 from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException, status
 
 router = APIRouter()
 
+
+email_sender = 'ejibe750@gmail.com'
+email_password = config('MAIL_PASSWORD')
+
+email_receiver = 'vanedi2754@cosaxu.com'
+
+subject = 'Test'
+
+body = """
+testing testing
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)
+
+
+context = ssl.create_default_context()
+
 @router.post("/gmail_sendmessage")
 def gmail_send_message():
-    """Create and send an email message
-    Print the returned  message id
-    Returns: Message object, including message id
-
-    Load pre-authorized user credentials from the environment.
-    TODO(developer) - See https://developers.google.com/identity
-    for guides on implementing OAuth2 for the application.
     """
-    creds, _ = google.auth.default()
+    Create and send an email message
 
-    try:
-        service = build('gmail', 'v1', credentials=creds)
-        message = EmailMessage()
+    """
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+    return {"Message":"Your message has been sent"}
 
-        message.set_content('This is automated draft mail')
-
-        message['To'] = 'mmachiejibe@yahoo.com'
-        message['From'] = 'mmachiejibe@gmail.com'
-        message['Subject'] = 'Automated draft'
-
-        # encoded message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
-            .decode()
-
-        create_message = {
-            'raw': encoded_message
-        }
-        # pylint: disable=E1101
-        send_message = (service.users().messages().send
-                        (userId="me", body=create_message).execute())
-        print(F'Message Id: {send_message["id"]}')
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-        send_message = None
-    return send_message
+ 
