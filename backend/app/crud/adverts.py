21051062d992838f0b4e2c5bbbcd74e6d18b8c25
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-12-09 01:46:38 +0100

    created GET endpoint for all ads & single ads

--- backend/app/crud/adverts.py
+++ backend/app/crud/adverts.py
@@ -27,4 +27,14 @@ class CRUDAdvert(CRUDBase[Advert, AdvertCreate, AdvertUpdate]):
         db.refresh(db_oj)
         return db_oj
 
+
+
+    def get_all_adverts(self, db: Session):
+        """Returns all adverts"""
+        return db.query(Advert).all() 
+
+    def get_adverts_by_id(self, db:Session, id) -> Any:  
+        """gets adverts by id"""
+        return db.query(Advert).filter(Advert.id == id).first()      
+
 advert=CRUDAdvert(Advert)
