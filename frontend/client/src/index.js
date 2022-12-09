3e2d76b9b0e28a860cb9be1ce41d47bbc52bcfc0
Hack-Light <onohsomtochukwubasil@gmail.com>
2022-12-09 19:00:06 +0100

    fix

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -7,18 +7,21 @@ import reportWebVitals from "./reportWebVitals";
 import { store } from "./redux/store";
 import "./translation/i18n";
 
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import * as atatus from 'atatus-spa';
+atatus.config('6cc0bcd74dfc46d1ac3057040240ae69').install();
 
-process.env.NODE_ENV === "production" && Sentry.init({
-  dsn: "https://b8eb1a11ad654e99994c510a60ddb487@o4504276826914816.ingest.sentry.io/4504278718152704",
-  integrations: [new BrowserTracing()],
+// import * as Sentry from "@sentry/react";
+// import { BrowserTracing } from "@sentry/tracing";
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+// process.env.NODE_ENV === "production" && Sentry.init({
+//   dsn: "https://b8eb1a11ad654e99994c510a60ddb487@o4504276826914816.ingest.sentry.io/4504278718152704",
+//   integrations: [new BrowserTracing()],
+
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 1.0,
+// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
