619f5e7b1dc82dc31259baf2d1e700e677c66e85
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-12-11 02:04:50 +0100

    translated the compliance page

--- frontend/client/src/Pages/Compliance/components/ComplianceLayout.jsx
+++ frontend/client/src/Pages/Compliance/components/ComplianceLayout.jsx
@@ -1,56 +1,31 @@
-import React from "react";
-import { Box, useTheme } from "@mui/material";
-import { ComplianceSection } from "./complianceStyle";
+import React from 'react';
+import { Box, useTheme } from '@mui/material';
+import { ComplianceSection } from './complianceStyle';
+import { useTranslation } from 'react-i18next';
 
 const ComplianceLayout = () => {
-  const theme = useTheme()
-  const darkMode = theme.palette.mode === "dark"
-  return (
-    <Box display="flex" backgroundColor={darkMode ? "#00050C" : ""}>
-      <ComplianceSection>
-        <div className="">
-          <h2>COMPLIANCE</h2>
-          <h1>StreetRate Compliance</h1>
-          <p>
-            Let’s inform you of our compliance and how it is important for your
-            activity on StreetRate
-          </p>
-          <div className="section-body">
-            <p>
-              Our long-standing record for our in-depth compliance monitoring
-              and programs makes us a leader in delivering global currency
-              exchange and payments services. Our compliance program is core to
-              us as a team and very important for our esteemed users. At the
-              core of our system, we use strict Know Your Customer(KYC) best
-              practices, and a prudent account opening and monitoring process.
-            </p>
-            <p>
-              We understand the importance and dutifully accept the
-              responsibilities that come with working within the financial
-              industry. Businesses can be targeted by groups looking to launder
-              money or other illegal activities such as fraud, theft, or
-              trafficking. Our entire team is committed to aggressively
-              deterring criminals from involving the company in their practices.
-            </p>
-            <p>
-              We employ annual independent reviews of our compliance training
-              and programs for additional layers of security. StreetRate is
-              dedicated to diligent compliance practices and developing
-              industry-leading compliance tools.
-            </p>
-            <p>
-              In lieu to this, we have obtained certificates and licenses that
-              make a credible, legit and sure.
-            </p>
-            <p>
-              All this is aimed at ensuring users are safe and comfortable on
-              our platform.
-            </p>
-          </div>
-        </div>
-      </ComplianceSection>
-    </Box>
-  );
+	const theme = useTheme();
+	const darkMode = theme.palette.mode === 'dark';
+
+	const { t } = useTranslation();
+	return (
+		<Box display='flex' backgroundColor={darkMode ? '#00050C' : ''}>
+			<ComplianceSection>
+				<div className=''>
+					<h2>{t('compliance_heading_two')}</h2>
+					<h1>{t('compliance_heading_one')}</h1>
+					<p>{t('compliance_inform')}</p>
+					<div className='section-body'>
+						<p>{t('compliance_record')}</p>
+						<p>{t('compliance_crime_prevent')}</p>
+						<p>{t('compliance_review')}</p>
+						<p>{t('compliance_cert')}</p>
+						<p>{t('compliance_safety')}</p>
+					</div>
+				</div>
+			</ComplianceSection>
+		</Box>
+	);
 };
 
 export default ComplianceLayout;
--- frontend/client/src/translation/translation-dictionary/index.js
+++ frontend/client/src/translation/translation-dictionary/index.js
@@ -1,41 +1,44 @@
 // Export all translations
 
-import navbar from "./component/navbar";
-import footer from "./component/footer";
-import hero from "./component/hero";
-import convert from "./component/convert";
-import home from "./pages/home";
+import navbar from './component/navbar';
+import footer from './component/footer';
+import hero from './component/hero';
+import convert from './component/convert';
+import home from './pages/home';
+import compliance from './pages/compliance';
 
 const resources = {};
 
 const Translations = [
-  // Pages
-  ...home,
-  // Components
-  ...navbar,
-  ...footer,
-  ...hero,
-  ...convert,
+	// Pages
+	...home,
+	...compliance,
+
+	// Components
+	...navbar,
+	...footer,
+	...hero,
+	...convert,
 ];
 
 Translations.forEach((o) => {
-  Object.keys(o).forEach((key) => {
-    let langCode = String;
-    let langKey = String;
-    let langKeyVal = String;
+	Object.keys(o).forEach((key) => {
+		let langCode = String;
+		let langKey = String;
+		let langKeyVal = String;
 
-    if (key !== "key") {
-      langCode = key;
-      langKey = o.key;
-      langKeyVal = o[key];
+		if (key !== 'key') {
+			langCode = key;
+			langKey = o.key;
+			langKeyVal = o[key];
 
-      if (resources[langCode] === undefined) {
-        resources[langCode] = { translation: {} };
-      }
+			if (resources[langCode] === undefined) {
+				resources[langCode] = { translation: {} };
+			}
 
-      resources[langCode].translation[langKey] = langKeyVal;
-    }
-  });
+			resources[langCode].translation[langKey] = langKeyVal;
+		}
+	});
 });
 
 export default resources;
