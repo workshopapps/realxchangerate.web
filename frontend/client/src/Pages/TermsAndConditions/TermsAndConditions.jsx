c7dc196ffdbf966b98c37c4473668806b356abba
Aksanti Bahiga tacite <71480535+marcellintacite@users.noreply.github.com>
2022-12-12 02:11:28 +0300

    Fix: Merge conflict

--- frontend/client/src/Pages/TermsAndConditions/TermsAndConditions.jsx
+++ frontend/client/src/Pages/TermsAndConditions/TermsAndConditions.jsx
@@ -10,60 +10,62 @@ const TermsAndConditions = () => {
 
   const textColor = DarkMode ? "#FAFAFA" : "";
   return (
-<Box backgroundColor={DarkMode ? "#131825" : ""}>
-    <Box
-      sx={{
-        maxWidth: { xs: "90%", lg: "84%" },
-        marginInline: "auto",
-      }}
-    >
-      <div style={{ marginBlockEnd: "60px" }}>
-        <PageTitle style={{ color: textColor }}>{t("term_title")}</PageTitle>
+    <Box backgroundColor={DarkMode ? "#131825" : ""}>
+      <Box
+        sx={{
+          maxWidth: { xs: "90%", lg: "84%" },
+          marginInline: "auto",
+        }}
+        backgroundColor={DarkMode ? "#131825" : ""}
+      >
+        <div style={{ marginBlockEnd: "60px" }}>
+          <PageTitle style={{ color: textColor }}>{t("term_title")}</PageTitle>
 
-        <TermsWrapper>
-          <h2 style={{ color: textColor }}>{t("term_sub_title")}</h2>
-          <p>{t("term_p1")}</p>
-          <p>{t("term_p2")}</p>
-          <p>{t("term_p3")}</p>
+          <TermsWrapper>
+            <h2 style={{ color: textColor }}>{t("term_sub_title")}</h2>
+            <p style={{ color: textColor }}>{t("term_p1")}</p>
+            <p style={{ color: textColor }}>{t("term_p2")}</p>
+            <p style={{ color: textColor }}>{t("term_p3")}</p>
 
-          <h4 style={{ color: textColor, marginBlock: "30px 0px" }}>
-            {t("term_sub_1")}
-          </h4>
-          <p style={{ color: textColor }}>{t("term_p4")}</p>
+            <h4 style={{ color: textColor, marginBlock: "30px 0px" }}>
+              {t("term_sub_1")}
+            </h4>
+            <p style={{ color: textColor }}>{t("term_p4")}</p>
 
-          <h5 style={{ color: textColor }}>{t("term_subtitle_2")}</h5>
-          <p style={{ color: textColor }}>{t("term_p6")}</p>
+            <h5 style={{ color: textColor }}>{t("term_subtitle_2")}</h5>
+            <p style={{ color: textColor }}>{t("term_p6")}</p>
 
-          <h5 style={{ color: textColor }}>{t("term_sub_2")}</h5>
-          <p style={{ color: textColor }}>{t("term_p6")}</p>
+            <h5 style={{ color: textColor }}>{t("term_sub_2")}</h5>
+            <p style={{ color: textColor }}>{t("term_p6")}</p>
 
-          <h4 style={{ color: textColor, marginBlock: "30px 0px" }}>
-            {t("term_sub_3")}
-          </h4>
+            <h4 style={{ color: textColor, marginBlock: "30px 0px" }}>
+              {t("term_sub_3")}
+            </h4>
 
-          <h5 style={{ color: textColor }}>{t("term_sub_3")}</h5>
-          <p style={{ color: textColor }}>{t("term_p7")}</p>
+            <h5 style={{ color: textColor }}>{t("term_sub_3")}</h5>
+            <p style={{ color: textColor }}>{t("term_p7")}</p>
 
-          <h5 style={{ color: textColor }}>{t("term_sub_4")}</h5>
-          <p style={{ color: textColor }}>{t("term_p8")}</p>
+            <h5 style={{ color: textColor }}>{t("term_sub_4")}</h5>
+            <p style={{ color: textColor }}>{t("term_p8")}</p>
 
-          <h4 style={{ color: textColor, marginBlock: "30px 0px" }}>
-            {t("term_sub_4")}
-          </h4>
-          <p style={{ color: textColor }}>{t("term_p9")}</p>
-        </TermsWrapper>
+            <h4 style={{ color: textColor, marginBlock: "30px 0px" }}>
+              {t("term_sub_4")}
+            </h4>
+            <p style={{ color: textColor }}>{t("term_p9")}</p>
+          </TermsWrapper>
 
-        {/* style(STR-17): fixes aligment and spacing */}
+          {/* style(STR-17): fixes aligment and spacing */}
 
-        <TermsWrapper>
-          <h2 style={{ color: textColor, marginBlock: "60px 20px" }}>
-            {t("term_sub_5")}
-          </h2>
-          <p style={{ color: textColor }}>{t("term_p10")}</p>
-          <p style={{ color: textColor }}>{t("term_p11")}</p>
-          <p style={{ color: textColor }}>{t("term_p12")}</p>
-          <p style={{ color: textColor }}>{t("term_p13")}</p>
-        </TermsWrapper>
+          <TermsWrapper>
+            <h2 style={{ color: textColor, marginBlock: "60px 20px" }}>
+              {t("term_sub_5")}
+            </h2>
+            <p style={{ color: textColor }}>{t("term_p10")}</p>
+            <p style={{ color: textColor }}>{t("term_p11")}</p>
+            <p style={{ color: textColor }}>{t("term_p12")}</p>
+            <p style={{ color: textColor }}>{t("term_p13")}</p>
+          </TermsWrapper>
+        </div>
       </Box>
     </Box>
   );
