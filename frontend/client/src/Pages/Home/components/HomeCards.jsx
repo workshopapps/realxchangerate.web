01438b82ab35277cf3fecf6157799993e9266703
tacite <tacite.bahiga@gmail.com>
2022-12-11 17:49:03 +0300

    Feat: Translating HomeCards compontent

--- frontend/client/src/Pages/Home/components/HomeCards.jsx
+++ frontend/client/src/Pages/Home/components/HomeCards.jsx
@@ -4,8 +4,10 @@ import image1 from "../assets/home1.svg";
 import image2 from "../assets/home2.svg";
 import image3 from "../assets/home3.svg";
 import Cards from "./Cards";
+import { useTranslation } from "react-i18next";
 
 function HomeCards() {
+  const { t } = useTranslation();
   return (
     <Box
       sx={{
@@ -18,17 +20,14 @@ function HomeCards() {
         m: "5rem auto",
       }}
     >
+      <Cards text={t("home_card_one")} image={image1} />
       <Cards
-        text="Check out parallel market and bank exchange rates"
-        image={image1}
-      />
-      <Cards
-        text="Compare and contrast a wide range of currencies across the world"
+        text={t("home_card_two")}
         image={image2}
         bg="#00296B"
         color="#fff"
       />
-      <Cards text="Get the best deals and exchange your money" image={image3} />
+      <Cards text={t("home_card_three")} image={image3} />
     </Box>
   );
 }
