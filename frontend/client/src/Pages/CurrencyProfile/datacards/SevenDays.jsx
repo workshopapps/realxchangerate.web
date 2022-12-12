ece794d2a7211455970a8a3172a5dd2c4ca03576
EngrZaks <abzakariyya@gmail.com>
2022-12-12 23:39:24 +0100

    change color with change in rate for currency profile and fix flex

--- frontend/client/src/Pages/CurrencyProfile/datacards/SevenDays.jsx
+++ frontend/client/src/Pages/CurrencyProfile/datacards/SevenDays.jsx
@@ -8,7 +8,15 @@ function SevenDays({ rate }) {
       <Typography variant="h6"> Change (7d)</Typography>
       <Typography
         variant="h5"
-        sx={{ fontWeight: "bold", color: Math.sign(rate) === -1 && "#EF4444" }}
+        sx={{
+          fontWeight: "bold",
+          color:
+            Math.sign(rate) === -1
+              ? "#EF4444"
+              : Math.sign(rate) === 1
+              ? "#10B981"
+              : "",
+        }}
       >
         {rate} %
       </Typography>
