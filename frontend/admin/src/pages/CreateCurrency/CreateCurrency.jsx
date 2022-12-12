65511e5a9d66a172937cf63c3881faba52620292
damaestro165 <damaestro165@gmail.com>
2022-12-12 03:35:43 +0100

    feat:Add functionality  Rates to currency overview tabla

--- frontend/admin/src/pages/CreateCurrency/CreateCurrency.jsx
+++ frontend/admin/src/pages/CreateCurrency/CreateCurrency.jsx
@@ -1,34 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-import { MainWrapper, Container, Integration } from "./currStyles";
-import arrow from "../../assets/arrow_forward.svg";
+import { MainWrapper, Container, Integration } from './currStyles';
+import arrow from '../../assets/arrow_forward.svg';
 
-import Card from "./Card";
+import Card from './Card';
 
-import AddModalUi from "./AddModal";
-import EditModalUi from "./EditModal";
-import { Button, IconButton, Skeleton } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { getTrending } from "../../store/actions/dashboardActions";
-import Flag from "react-world-flags";
-import rafiki from "../../assets/rafiki.png";
-import cuate from "../../assets/cuate.png";
-import contact from "../../assets/contact.png";
-import styled from "styled-components";
-import MenuDrop from "../../component/MenuDrop/MenuDrop";
-import { getBaseDetails, getRates } from "../../store/actions/rateActions";
-import { toast } from "react-toastify";
-import Menu from "@mui/material/Menu";
-import more from "../../assets/more.svg";
-import MenuItem from "@mui/material/MenuItem";
+import AddModalUi from './AddModal';
+import EditModalUi from './EditModal';
+import AddRatesModalUi from './AddRatesModal';
+import { Button, IconButton, Skeleton } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTrending } from '../../store/actions/dashboardActions';
+import Flag from 'react-world-flags';
+import rafiki from '../../assets/rafiki.png';
+import cuate from '../../assets/cuate.png';
+import contact from '../../assets/contact.png';
+import styled from 'styled-components';
+import MenuDrop from '../../component/MenuDrop/MenuDrop';
+import { getBaseDetails, getRates } from '../../store/actions/rateActions';
+import { toast } from 'react-toastify';
+import Menu from '@mui/material/Menu';
+import more from '../../assets/more.svg';
+import MenuItem from '@mui/material/MenuItem';
 // import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -44,11 +45,11 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(even)": {
+  '&:nth-of-type(even)': {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  "&:last-child td, &:last-child th": {
+  '&:last-child td, &:last-child th': {
     border: 0,
   },
 }));
@@ -56,13 +57,15 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 export default function CreateCurrency() {
   const [open, setOpen] = useState(false);
   const [editVal, setEditVal] = useState({
-    country: "",
-    isocode: "",
-    symbol: "",
-    name: "",
+    country: '',
+    isocode: '',
+    symbol: '',
+    name: '',
   });
 
   const [editOpen, setEditOpen] = useState(false);
+  const [addRate, setAddRate] = useState(false);
+
   const [currencyNo, setCurrencyNo] = useState(10);
   const [viewMore, setViewMore] = useState(true);
   const [currs, setCurrs] = useState([]);
@@ -90,9 +93,9 @@ export default function CreateCurrency() {
   useEffect(() => {
     dispatch(getTrending());
 
-    dispatch(getBaseDetails("USD"));
+    dispatch(getBaseDetails('USD'));
 
-    dispatch(getRates("USD"));
+    dispatch(getRates('USD'));
 
     //eslint-disable-next-line
   }, [dispatch]);
@@ -107,6 +110,13 @@ export default function CreateCurrency() {
     setOpen(!open);
   };
 
+  const handleAddRate = () => {
+    setAddRate(!addRate);
+  };
+  const handleAddRateClose = () => {
+    setAddRate(!addRate);
+  };
+
   const handleEditOpen = () => {
     setEditOpen(!editOpen);
   };
@@ -114,11 +124,11 @@ export default function CreateCurrency() {
     setEditOpen(!editOpen);
   };
 
-  if (currencyStatus === "failed" || requestStatus === "failed") {
+  if (currencyStatus === 'failed' || requestStatus === 'failed') {
     toast.error(error);
   }
   const cellSkeleton = (
-    <Skeleton variant="rectangular" sx={{ mb: 1 }} width="100%" height="40px" />
+    <Skeleton variant='rectangular' sx={{ mb: 1 }} width='100%' height='40px' />
   );
 
   return (
@@ -126,34 +136,34 @@ export default function CreateCurrency() {
       <h1>Dashboard</h1>
       <Integration>
         <Card
-          title={"Update FAQs"}
+          title={'Update FAQs'}
           desc={
-            "Edit, add and delete Frequently Asked Questions (FAQs) on the web app"
+            'Edit, add and delete Frequently Asked Questions (FAQs) on the web app'
           }
-          link={"Update FAQs"}
-          loc={"/admin/faqs"}
-          bg={"#F7F9FD"}
+          link={'Update FAQs'}
+          loc={'/admin/faqs'}
+          bg={'#F7F9FD'}
           img={rafiki}
         />
         <Card
-          title={"Resolve Complaints"}
-          desc={"Get to resolve all user complaints on the web app "}
-          link={"Resolve Complaints"}
-          loc={"/admin/complaints"}
-          bg={"#FCF7FD"}
+          title={'Resolve Complaints'}
+          desc={'Get to resolve all user complaints on the web app '}
+          link={'Resolve Complaints'}
+          loc={'/admin/complaints'}
+          bg={'#FCF7FD'}
           img={cuate}
         />
         <Card
-          title={"Contact us Responses"}
-          desc={"Get to give feedback to users who submit the Contact Us form"}
-          link={"Give feedback"}
-          loc={"/admin/contact"}
-          bg={"#F7FDF8"}
+          title={'Contact us Responses'}
+          desc={'Get to give feedback to users who submit the Contact Us form'}
+          link={'Give feedback'}
+          loc={'/admin/contact'}
+          bg={'#F7FDF8'}
           img={contact}
         />
       </Integration>
       <Container>
-        <div className="topSect">
+        <div className='topSect'>
           <div>
             <h1>Currency Overview</h1>
             <p>Create and edit currency exchange rates</p>
@@ -169,42 +179,48 @@ export default function CreateCurrency() {
             editOpen={editOpen}
             editVal={editVal}
           />
+          <AddRatesModalUi
+            handleAddRate={handleAddRate}
+            handleAddRateClose={handleAddRateClose}
+            addRate={addRate}
+            editVal={editVal}
+          />
         </div>
         <TableContainer
-          style={{ margin: "auto -10px", width: "auto", overflowY: "visible" }}
+          style={{ margin: 'auto -10px', width: 'auto', overflowY: 'visible' }}
           component={Paper}
         >
           <Table
             sx={{ minWidth: 500 }}
-            size="small"
-            aria-label="customized table"
+            size='small'
+            aria-label='customized table'
           >
-            <TableHead style={{ backgroundColor: "#f8fafc" }}>
+            <TableHead style={{ backgroundColor: '#f8fafc' }}>
               <TableRow>
-                <StyledTableCell sx={{ fontWeight: "700" }}>
+                <StyledTableCell sx={{ fontWeight: '700' }}>
                   Country
                 </StyledTableCell>
-                <StyledTableCell align="left" sx={{ fontWeight: "700" }}>
+                <StyledTableCell align='right' sx={{ fontWeight: '700' }}>
                   Code
                 </StyledTableCell>
-                <StyledTableCell align="left" sx={{ fontWeight: "700" }}>
+                <StyledTableCell align='right' sx={{ fontWeight: '700' }}>
                   {`Rate/${baseCurrencyDetails.symbol}`}
                 </StyledTableCell>
-                <StyledTableCell align="right">
+                <StyledTableCell align='right'>
                   {/* Carbs&nbsp;(g) */}
                 </StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {requestStatus === "pending" ? (
+              {requestStatus === 'pending' ? (
                 <>
                   {Array.from(Array(3)).map((_, index) => (
                     <TableRow
                       key={index}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
                       {Array.from(Array(3)).map((_, index) => (
-                        <TableCell key={index} component="th" scope="row">
+                        <TableCell key={index} component='th' scope='row'>
                           {cellSkeleton}
                         </TableCell>
                       ))}
@@ -220,39 +236,39 @@ export default function CreateCurrency() {
                   .map((row, index) => (
                     <StyledTableRow key={row.country}>
                       <StyledTableCell
-                        component="th"
-                        scope="row"
-                        style={{ color: "rgba(71, 85, 105, 1)" }}
+                        component='th'
+                        scope='row'
+                        style={{ color: 'rgba(71, 85, 105, 1)' }}
                       >
-                        <div style={{ display: "flex", alignItems: "center" }}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
                           <Flag
                             code={row?.isocode?.slice(0, 2)}
-                            style={{ width: "30px", marginRight: "10px" }}
+                            style={{ width: '30px', marginRight: '10px' }}
                           />
                           <span>{row.country}</span>
                         </div>
                       </StyledTableCell>
                       <StyledTableCell
-                        align="left"
-                        style={{ color: "rgba(71, 85, 105, 1)" }}
+                        align='right'
+                        style={{ color: 'rgba(71, 85, 105, 1)' }}
                       >
                         {row.isocode}
                       </StyledTableCell>
                       <StyledTableCell
-                        align="left"
-                        style={{ color: "rgba(71, 85, 105, 1)" }}
+                        align='right'
+                        style={{ color: 'rgba(71, 85, 105, 1)' }}
                       >
-                        {currs[index]?.status === "fulfilled"
+                        {currs[index]?.status === 'fulfilled'
                           ? currs[index]?.value?.data?.parallel_total
-                          : "-"}
+                          : '-'}
                       </StyledTableCell>
                       <StyledTableCell
-                        align="right"
-                        style={{ color: "rgba(71, 85, 105, 1)" }}
+                        align='right'
+                        style={{ color: 'rgba(71, 85, 105, 1)' }}
                       >
                         <MenuDrop
                           handleEditOpen={handleEditOpen}
-                          handleOpen={handleOpen}
+                          handleAddRate={handleAddRate}
                           rowData={row}
                           setRowData={setEditVal}
                         />
@@ -265,12 +281,12 @@ export default function CreateCurrency() {
         </TableContainer>
         {viewMore && (
           <Button
-            variant="text"
-            sx={{ my: 2, textTransform: "capitalize", fontSize: "0.8rem" }}
+            variant='text'
+            sx={{ my: 2, textTransform: 'capitalize', fontSize: '0.8rem' }}
             onClick={handleViewMore}
           >
-            view more currencies{" "}
-            <img src={arrow} alt="arrow" style={{ marginLeft: "10px" }} />
+            view more currencies{' '}
+            <img src={arrow} alt='arrow' style={{ marginLeft: '10px' }} />
           </Button>
         )}
       </Container>
