8a7e183b20929f519e3944fc1d9f669927ddddf4
kelani34 <kelanitaiwo34@gmail.com>
2022-12-12 22:24:41 +0100

    update: homepage table

--- frontend/client/src/Pages/CurrencyProfile/CurrencyProfile.jsx
+++ frontend/client/src/Pages/CurrencyProfile/CurrencyProfile.jsx
@@ -1,257 +1,134 @@
-import React from 'react';
-import { Container, Typography, Grid, List, ListItem } from '@mui/material';
-import { Box } from '@mui/material';
-import { FormControl, MenuItem, Select } from '@mui/material';
-import { Button, StyledSelection } from './CurrencyProfile.styles';
-import chart from './assets/chart.png';
-import flag from './assets/flag.png';
-import mobile from './assets/mobile.png';
-import { currenciesList, countries } from '../../utils/data';
-import Table2 from '../Home/components/Table2';
-import CircularProgressWithLabel from '@mui/material/CircularProgress';
-import DeleteIcon from './assets/delete.svg';
-import { useState, useEffect } from 'react';
-import Menu from '@mui/material/Menu';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
-import { useSelector } from 'react-redux';
-import { dispatch } from '../../redux/store';
-import Countdown from 'react-countdown-simple';
-import { GetCurrencyRates } from '../../redux/features/Reducers/serviceActions';
-import { tableCurrenciesList } from '../Home/data';
-import { AddCircle } from 'iconsax-react';
-import Chart from './chart/Chart';
-import DataCards from './datacards/DataCards';
-import dayjs from 'dayjs';
+import React from "react";
+import { Container, Typography, Grid, List, ListItem } from "@mui/material";
+import { Box } from "@mui/material";
+import { FormControl, MenuItem, Select } from "@mui/material";
+import { Button, StyledSelection } from "./CurrencyProfile.styles";
+import chart from "./assets/chart.png";
+import flag from "./assets/flag.png";
+import mobile from "./assets/mobile.png";
+import { currenciesList, countries } from "../../utils/data";
+import Table2 from "../Home/components/Table2";
+import CircularProgressWithLabel from "@mui/material/CircularProgress";
+import DeleteIcon from "./assets/delete.svg";
+import { useState, useEffect } from "react";
+import Menu from "@mui/material/Menu";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
+import { useSelector } from "react-redux";
+import { dispatch } from "../../redux/store";
+import Countdown from "react-countdown-simple";
+import { GetCurrencyRates } from "../../redux/features/Reducers/serviceActions";
+import { tableCurrenciesList } from "../Home/data";
+import { AddCircle } from "iconsax-react";
+import Chart from "./chart/Chart";
+import DataCards from "./datacards/DataCards";
+import dayjs from "dayjs";
+import Liverates from "../Home/Liverates";
 //
 //
 const CurrencyProfile = () => {
-	//
-	const [currency, setCurrency] = React.useState('NGN');
-	//
-	const oneHour = new Date(
-		new Date().setHours(new Date().getHours() + 2)
-	).toISOString();
-	const { currencyRates, currencyList } = useSelector((state) => state.service);
-	// const progressUpdate = document.getElementById("time").innerText;
-	const [currencies, setCurrencies] = useState(tableCurrenciesList);
-	const [getCurrency, setGetCurrency] = useState([]);
-	const [dateUpdate, setDateUpdate] = useState('');
-	//
-	const handleEdit = () => {
-		toggle();
-	};
-	useEffect(() => {
-		if (currencyList.length > 0) {
-			dispatch(GetCurrencyRates(currencyList));
-		}
+  //
+  const [currency, setCurrency] = React.useState("NGN");
+  //
+  const oneHour = new Date(
+    new Date().setHours(new Date().getHours() + 2)
+  ).toISOString();
+  const { currencyRates, currencyList } = useSelector((state) => state.service);
+  // const progressUpdate = document.getElementById("time").innerText;
+  const [currencies, setCurrencies] = useState(tableCurrenciesList);
+  const [getCurrency, setGetCurrency] = useState([]);
+  const [dateUpdate, setDateUpdate] = useState("");
+  //
+  const handleEdit = () => {
+    toggle();
+  };
+  useEffect(() => {
+    if (currencyList.length > 0) {
+      dispatch(GetCurrencyRates(currencyList));
+    }
 
-		fetch('https://api.streetrates.hng.tech/api/currency/currencies/flags')
-			.then((response) => response.json())
-			.then((data) => {
-				setGetCurrency(data);
-				setDateUpdate(data[0].rate.last_updated);
-			})
-			.catch((e) => console.log(e));
+    fetch("https://api.streetrates.hng.tech/api/currency/currencies/flags")
+      .then((response) => response.json())
+      .then((data) => {
+        setGetCurrency(data);
+        setDateUpdate(data[0].rate.last_updated);
+      })
+      .catch((e) => console.log(e));
 
-		//eslint-disable-next-line
-	}, [currencyList]);
-	const handleDelete = (e) => {
-		setCurrencies(currencies.filter((item) => item.id !== e));
-	};
-	function toggle() {
-		const button = document.getElementById('edit');
-		const deleteIcons = document.querySelectorAll('.delete-cur');
+    //eslint-disable-next-line
+  }, [currencyList]);
+  const handleDelete = (e) => {
+    setCurrencies(currencies.filter((item) => item.id !== e));
+  };
+  function toggle() {
+    const button = document.getElementById("edit");
+    const deleteIcons = document.querySelectorAll(".delete-cur");
 
-		if (button.innerHTML === 'Done') {
-			button.innerHTML = 'Edit';
-			deleteIcons.forEach((e) => (e.style.display = 'none'));
-		} else {
-			button.innerHTML = 'Done';
-			deleteIcons.forEach((e) => (e.style.display = 'block'));
-		}
-	}
+    if (button.innerHTML === "Done") {
+      button.innerHTML = "Edit";
+      deleteIcons.forEach((e) => (e.style.display = "none"));
+    } else {
+      button.innerHTML = "Done";
+      deleteIcons.forEach((e) => (e.style.display = "block"));
+    }
+  }
 
-	const handleAdd = (closeModal, item) => {
-		setCurrencies([...currencies, item]);
-		closeModal();
-	};
-	//
+  const handleAdd = (closeModal, item) => {
+    setCurrencies([...currencies, item]);
+    closeModal();
+  };
+  //
 
-	//
-	// const CurrencyMenu = (props) => {
-	//   const { currency } = props;
-	//   const countryDetails = countries.filter(
-	//     (countr) => countr.label === currency.country
-	//   );
-	//   return (
-	//     <MenuItem
-	//       key={currency.isocode}
-	//       value={currency.isocode}
-	//       sx={{ display: "flex", gap: "1rem" }}
-	//     >
-	//       <Box>
-	//         <img
-	//           loading="lazy"
-	//           width="20"
-	//           src={`https://flagcdn.com/w20/${countryDetails[0].code.toLowerCase()}.png`}
-	//           srcSet={`https://flagcdn.com/w40/${countryDetails[0].code.toLowerCase()}.png 2x`}
-	//           alt=""
-	//         />
-	//       </Box>
-	//       <Box>{currency.country}</Box>
-	//       <Box>({currency.isocode})</Box>
-	//     </MenuItem>
-	//   );
-	// };
-	//
-	//
-	return (
-		<Container
-			style={{
-				maxWidth: '1440px',
-			}}
-			sx={{
-				paddingLeft: { xs: 2, sm: 2, md: 2, lg: 9, xl: 9 },
-				paddingRight: { xs: 2, sm: 2, md: 2, lg: 9, xl: 9 },
-			}}>
-			<Box style={{ margin: '90px auto' }}>
-				<Chart />
-			</Box>
+  //
+  // const CurrencyMenu = (props) => {
+  //   const { currency } = props;
+  //   const countryDetails = countries.filter(
+  //     (countr) => countr.label === currency.country
+  //   );
+  //   return (
+  //     <MenuItem
+  //       key={currency.isocode}
+  //       value={currency.isocode}
+  //       sx={{ display: "flex", gap: "1rem" }}
+  //     >
+  //       <Box>
+  //         <img
+  //           loading="lazy"
+  //           width="20"
+  //           src={`https://flagcdn.com/w20/${countryDetails[0].code.toLowerCase()}.png`}
+  //           srcSet={`https://flagcdn.com/w40/${countryDetails[0].code.toLowerCase()}.png 2x`}
+  //           alt=""
+  //         />
+  //       </Box>
+  //       <Box>{currency.country}</Box>
+  //       <Box>({currency.isocode})</Box>
+  //     </MenuItem>
+  //   );
+  // };
+  //
+  //
+  return (
+    <Container
+      style={{
+        maxWidth: "1440px",
+        display: "flex",
+        gap: "30px",
+        maxWidth: "1440px",
+        flexDirection: "column",
+      }}
+      sx={{
+        paddingLeft: { xs: 2, sm: 2, md: 2, lg: 9, xl: 9 },
+        paddingRight: { xs: 2, sm: 2, md: 2, lg: 9, xl: 9 },
+      }}
+    >
+      <Box>
+        <Chart />
+      </Box>
 
-			<DataCards />
+      <DataCards />
 
-			<div
-				style={{
-					textAlign: 'end',
-					margin: '30px 0px',
-				}}>
-				<Button
-					style={{
-						border: '1px solid #0062ff',
-						marginRight: '0',
-						marginLeft: 'auto',
-					}}
-					id='edit'
-					color='#0062ff'
-					bg='#fff'
-					padding='50px'
-					sx={{
-						display: { xs: 'none', md: 'block' },
-					}}
-					onClick={() => handleEdit()}>
-					Edit
-				</Button>
-			</div>
-			<List
-				style={{
-					border: '1px solid #CBD5E1',
-					padding: '0',
-					borderRadius: '5px',
-				}}>
-				<ListItem
-					sx={{
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'space-between',
-						bgcolor: '#F1F5F9',
-						fontSize: '1.7rem',
-						p: '1.5rem',
-						fontWeight: 600,
-						color: 'black',
-					}}>
-					<Box>Currency</Box>
-					<Box>Parallel</Box>
-					<Box>Bank</Box>
-				</ListItem>
-				{currencies.map((currency, index) => {
-					return (
-						<>
-							{currencyRates.length > 0 && (
-								<Table2
-									isocode={currency?.isocode}
-									country={currency?.country}
-									key={index}
-									rates={
-										currencyRates.find(
-											(x) => x?.currency?.isocode === currency?.isocode
-										).rate
-									}
-									link={currency?.link}
-									symbol={currency?.symbol}
-									flag={currency?.flag}
-									deleteIcon={
-										<img
-											src={DeleteIcon}
-											className='delete-cur'
-											alt=''
-											style={{ display: 'none' }}
-											onClick={(e) => handleDelete(currency?.id)}
-										/>
-									}
-								/>
-							)}
-						</>
-					);
-				})}
-			</List>
-
-			<StyledSelection>
-				<PopupState variant='popover' popupId='demo-popup-menu'>
-					{(popupState) => (
-						<React.Fragment>
-							<div style={{ cursor: 'pointer' }} {...bindTrigger(popupState)}>
-								<div className='add'>
-									<AddCircle size={24} variant='Bold' color='#0062ff' />
-									<span className='addspan'>Add currency</span>
-								</div>
-							</div>
-							<Menu {...bindMenu(popupState)}>
-								{getCurrency.map((item, index) => {
-									return (
-										<MenuItem
-											onClick={() => handleAdd(popupState.close, item)}
-											key={index}>
-											<img
-												loading='lazy'
-												width='20'
-												src={`${item.flag}`}
-												alt=''
-												style={{ marginRight: '10px' }}
-											/>
-											{item.isocode} - {item.country}
-										</MenuItem>
-									);
-								})}
-							</Menu>
-						</React.Fragment>
-					)}
-				</PopupState>
-				<div className='lastUpdate'>
-					{/* <div style={{ position: "relative" }}>
-            <CircularProgressWithLabel
-              variant="determinate"
-              value={59}
-              thickness={6}
-            />
-            <i>
-              <Countdown
-                targetDate={oneHour}
-                renderer={({ days, hours, minutes, seconds }) => (
-                  <span id="time" >{minutes}</span>
-                )}
-              />
-            </i>
-          </div> */}
-					<span>
-						Last updated{' '}
-						{dateUpdate
-							? dayjs(dateUpdate).format('h:mm A, MMM D')
-							: dayjs().format('h:mm A, MMM D')}
-					</span>
-				</div>
-			</StyledSelection>
-		</Container>
-	);
+      <Liverates />
+    </Container>
+  );
 };
 
 export default CurrencyProfile;
---
+++ frontend/client/src/Pages/Home/Liverates.jsx
@@ -0,0 +1,331 @@
+import React from "react";
+import { Box, List, ListItem, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+// import Table from "./components/Table";
+import Table2 from "./components/Table2";
+import { tableCurrenciesList } from "./data";
+import styled from "styled-components";
+import add from "./assets/add.svg";
+import DeleteIcon from "./assets/delete.png";
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
+import { useSelector } from "react-redux";
+import { dispatch } from "../../redux/store";
+import { GetCurrencyRates } from "../../redux/features/Reducers/serviceActions";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(name, calories, fat) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0),
+  createData("Ice cream sandwich", 237, 9.0),
+  createData("Eclair", 262, 16.0),
+  createData("Cupcake", 305, 3.7),
+  createData("Gingerbread", 356, 16.0),
+];
+
+function Liverates() {
+  const theme = useTheme();
+  const dark = theme.palette.mode === "dark";
+  const oneHour = new Date(
+    new Date().setHours(new Date().getHours() + 2)
+  ).toISOString();
+  const { currencyRates, currencyList } = useSelector((state) => state.service);
+  // const progressUpdate = document.getElementById("time").innerText;
+  const [currencies, setCurrencies] = useState(tableCurrenciesList);
+  const [getCurrency, setGetCurrency] = useState([]);
+  const [dateUpdate, setDateUpdate] = useState("");
+
+  // Adding translation
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (currencyList.length > 0) {
+      dispatch(GetCurrencyRates(currencyList));
+    }
+
+    fetch("https://api.streetrates.hng.tech/api/currency/currencies/flags")
+      .then((response) => response.json())
+      .then((data) => {
+        data.sort(function (a, b) {
+          const nameA = a.isocode.toUpperCase();
+          const nameB = b.isocode.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+        console.log(data);
+        setGetCurrency(data);
+        setDateUpdate(data[0].rate.last_updated);
+      })
+      .catch((e) => console.log(e));
+
+    //eslint-disable-next-line
+  }, [currencyList]);
+
+  const handleEdit = () => {
+    toggle();
+  };
+  const handleDelete = (e) => {
+    setCurrencies(currencies.filter((item) => item.id !== e));
+  };
+  function toggle() {
+    const button = document.getElementById("edit");
+    const deleteIcons = document.querySelectorAll(".delete-cur");
+
+    if (button.innerHTML === "Done") {
+      button.innerHTML = "Remove Currency";
+      deleteIcons.forEach((e) => (e.style.display = "none"));
+    } else {
+      button.innerHTML = "Done";
+      deleteIcons.forEach((e) => (e.style.display = "block"));
+    }
+  }
+
+  const handleAdd = (closeModal, item) => {
+    setCurrencies([...currencies, item]);
+    closeModal();
+  };
+  return (
+    <>
+      <StyledBox>
+        <TypographyLive
+          sx={{ color: "#0062FF", fontSize: "16px", fontWeight: 500 }}
+        >
+          {t("home_live")}
+        </TypographyLive>
+        <TypographyHead
+          component="h2"
+          sx={{ fontSize: "32px", maxWidth: "595px", fontWeight: 800 }}
+        >
+          {t("home_title")}
+        </TypographyHead>
+        {/* <StyledEdit className="action" id="edit" onClick={() => handleEdit()}>
+          {t("home_btn")}
+        </StyledEdit> */}
+      </StyledBox>
+
+      {/* Table  */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead
+            sx={{
+              backgroundColor: "#F1F5F9",
+            }}
+          >
+            <TableRow>
+              <TableCell style={{ fontSize: "1.7rem", fontWeight: "600" }}>
+                Currency
+              </TableCell>
+              <TableCell style={{ fontSize: "1.7rem", fontWeight: "600" }}>
+                parallel
+              </TableCell>
+              <TableCell style={{ fontSize: "1.7rem", fontWeight: "600" }}>
+                Bank
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {currencies.map((currency) => {
+              return (
+                <>
+                  {currencyRates.length > 0 && (
+                    <Table2
+                      country={currency.country}
+                      key={currency.id}
+                      rates={
+                        currencyRates.find(
+                          (x) => x.currency.isocode === currency.isocode
+                        ).rate
+                      }
+                      link={currency.link}
+                      symbol={currency.symbol}
+                      flag={currency.flag}
+                      isocode={currency.isocode}
+                      deleteIcon={
+                        <PopupState variant="popover" popupId="demo-popup-menu">
+                          {(popupState) => (
+                            <React.Fragment>
+                              <Button {...bindTrigger(popupState)}>
+                                <div className="add">
+                                  <img
+                                    src={DeleteIcon}
+                                    className="delete-cur"
+                                    style={{
+                                      // display: "none",
+                                      width: "16px",
+                                      margin: "auto",
+                                    }}
+                                    alt=""
+                                  />
+                                </div>
+                              </Button>
+                              <Menu {...bindMenu(popupState)}>
+                                <MenuItem
+                                  onClick={(e) => handleDelete(currency.id)}
+                                >
+                                  Delete
+                                </MenuItem>
+                                <MenuItem>
+                                  <Link
+                                    to={`/${currency.isocode}/currency-profile`}
+                                  >
+                                    View currency Profile
+                                  </Link>
+                                </MenuItem>
+                              </Menu>
+                            </React.Fragment>
+                          )}
+                        </PopupState>
+                      }
+                    />
+                  )}
+                </>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <StyledSelection>
+        <PopupState variant="popover" popupId="demo-popup-menu">
+          {(popupState) => (
+            <React.Fragment>
+              <Button {...bindTrigger(popupState)}>
+                <div className="add">
+                  <img src={add} alt="" />
+                  <span className="addspan">{t("home_add")}</span>
+                </div>
+              </Button>
+              <Menu {...bindMenu(popupState)}>
+                {getCurrency.map((item) => {
+                  return (
+                    <MenuItem
+                      onClick={() => handleAdd(popupState.close, item)}
+                      key={item.id}
+                      sx={{
+                        backgroundColor: dark ? "#001E4E" : "",
+                        padding: "3",
+                      }}
+                    >
+                      <img
+                        loading="lazy"
+                        width="20"
+                        src={`${item.flag}`}
+                        alt=""
+                        style={{ marginRight: "10px" }}
+                      />
+                      {item.isocode} - {item.country}
+                    </MenuItem>
+                  );
+                })}
+              </Menu>
+            </React.Fragment>
+          )}
+        </PopupState>
+      </StyledSelection>
+    </>
+  );
+}
+
+export default Liverates;
+
+const TypographyLive = styled(Typography)`
+  @media screen and (max-width: 480px) {
+    text-align: center;
+  }
+`;
+
+const TypographyHead = styled(Typography)`
+  @media screen and (max-width: 480px) {
+    text-align: center;
+  }
+`;
+const StyledBox = styled(Box)`
+  width: 100%;
+`;
+
+const StyledBankBox = styled(Box)`
+  /* @media screen and (max-width: 480px) {
+    display: none;
+  } */
+`;
+
+const StyledEdit = styled.button`
+  border: 1px solid #0062ff;
+  border-radius: 8px;
+  background: none;
+  padding: 10px 22px;
+  gap: 12px;
+
+  width: 180px;
+  height: 42px;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 22px;
+  color: #0062ff;
+  margin-top: 26px;
+  float: right;
+`;
+
+const StyledSelection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 64px;
+  .add {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  .addspan {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    color: #0062ff;
+    @media screen and (max-width: 480px) {
+      font-size: 10px;
+    }
+  }
+  .lastUpdate {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    @media screen and (max-width: 480px) {
+      max-width: 50%;
+    }
+    i span {
+      position: absolute;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 22px;
+      right: 18px;
+      top: 9px;
+    }
+    span {
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 22px;
+      @media screen and (max-width: 480px) {
+        font-size: 10px;
+      }
+    }
+  }
+`;
