af04d63e00eb25a860ad42a3b70f15e770bc6e7e
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-12-13 12:46:52 +0100

    remove trending

--- frontend/admin/src/component/TrendingDataComponent/TrendingDataLayout.jsx
+++ frontend/admin/src/component/TrendingDataComponent/TrendingDataLayout.jsx
@@ -1,52 +1,22 @@
 import {
-	StyledGrid,
-	StyledCard,
-	StyledTrendCards,
-	StyledWrapper,
-} from './TrendingDataLayout.styled';
-import DataTable from './table/DataTable';
-import Chart from './chart/Chart';
+  StyledGrid,
+  StyledCard,
+  StyledTrendCards,
+  StyledWrapper,
+} from "./TrendingDataLayout.styled";
+import DataTable from "./table/DataTable";
+import Chart from "./chart/Chart";
 
 const data = {
-	tvl: '41.58b',
-	change: '1.32', //
+  tvl: "41.58b",
+  change: "1.32", //
 };
 const TrendingDataLayout = () => {
-	return (
-		<StyledWrapper>
-			<StyledGrid>
-				<Chart />
-				<StyledTrendCards>
-					{/* <StyledCard>
-						<h3>Total Value Locked (USD)</h3>
-						<p>${data.tvl}</p>
-					</StyledCard> */}
-
-					<StyledCard>
-						<h3>Change (1h)</h3>
-						<span>{data.change}%</span>
-					</StyledCard>
-
-					<StyledCard>
-						<h3>Change (24h)</h3>
-						<span>{data.change}%</span>
-					</StyledCard>
-
-					<StyledCard>
-						<h3>Change (7d)</h3>
-						<span>{data.change}%</span>
-					</StyledCard>
-
-					<StyledCard>
-						<h3>Change (1m)</h3>
-						<span>{data.change}%</span>
-					</StyledCard>
-				</StyledTrendCards>
-			</StyledGrid>
-
-			<DataTable />
-		</StyledWrapper>
-	);
+  return (
+    <StyledWrapper>
+      <DataTable />
+    </StyledWrapper>
+  );
 };
 
 export default TrendingDataLayout;
