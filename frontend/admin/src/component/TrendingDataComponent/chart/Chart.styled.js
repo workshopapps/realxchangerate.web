ad0c64774309be0c83aae2d750a541943aa4ffe4
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-12-13 10:55:29 +0100

    update

--- frontend/admin/src/component/TrendingDataComponent/chart/Chart.styled.js
+++ frontend/admin/src/component/TrendingDataComponent/chart/Chart.styled.js
@@ -4,7 +4,7 @@ export const StyledChartWrapper = styled.div`
 	padding: 14px 0px 14px 19px;
 	box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
 	border-radius: 16px;
-	border: 1px solid red;
+	// border: 1px solid red;
 	// flex-grow: 1;
 	display: flex;
 	flex-direction: column;
